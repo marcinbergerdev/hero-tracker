a66491a6973d5a4ff2688d467b1fd5d9dfa57cc1
marcinbergerdev <marcin.berger95@op.pl>
2023-12-01 15:49:47 +0100

    feat: add character data fetching from api

---
+++ store/getHeroes.ts
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+
+export const useGetHeroes = defineStore("getHeroes", () => {
+   const selectedHeroes = ref(null);
+   const isLoadingSpinner = ref(false);
+   const errorMessage = ref("");
+
+   const dataValidation = (response: Response) => {
+      if (!response.ok) {
+         throw new Error("Something goes wrong, try later!");
+      }
+   };
+
+   const setHeroes = async (urlPath: string ) => {
+      try {
+         isLoadingSpinner.value = true;
+
+         const response = await fetch(`https://api.gameofthronesquotes.xyz/v1/${urlPath}`);
+         dataValidation(response);
+
+         const data = await response.json();
+         selectedHeroes.value = data;
+
+      } catch (error: any) {
+         errorMessage.value = error.message;
+
+      } finally {
+         isLoadingSpinner.value = false;
+      }
+   };
+
+   return { selectedHeroes, isLoadingSpinner, errorMessage, setHeroes};
+});
